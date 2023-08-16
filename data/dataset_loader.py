dcbd5f7bf01c59890617b5f469598ae3b2daa82d
Ariel Tyson <ariel30190@hotmail.com>
2023-08-16 00:30:30 -0700

    Create file to download simple dataset using PyTorch's torchvision library

---
+++ data/dataset_loader.py
@@ -0,0 +1,8 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+
+train_dataset = torchvision.datasets.MNIST(root='./data', train=True, transform=transform, download=True)
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
